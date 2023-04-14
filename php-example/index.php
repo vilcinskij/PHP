54f8e5da0cb1a1309939959196a12c23157bac51
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-14 19:56:51 +0300

    new_files ž

--- php-example/index.php
+++ php-example/index.php
@@ -1,38 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>INDEX</title>
 </head>
-
 <body>
     <?php
 
-    $x = "0";
-    $y = "5";
-    $z = "6";
-
-    if ($z >= 0 && $x == 0) {
-
-
-        switch ($z) {
-            case 4;
-                echo "LABAS";
-                break;
-            case 5;
-                echo "LABAS_2";
-                break;
-            case 6;
-                echo "LABAS_3";
-                break;
-        }
-    } else
-        echo "arba $z mažiaus už 0 arba $x nelygu 0"
+echo "New PHP"
 
     ?>
 </body>
-
 </html>

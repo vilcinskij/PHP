7bb5d3e846033eb2370ee8fb8190ef437c23a589
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-15 02:30:48 +0300

    arrays

---
+++ php-example/arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARRAYS</title>
+</head>
+
+<body>
+
+    <?php
+    $arr = [0, 1, 5, 0, 9];
+    echo $arr[3];
+    
+    $arr2 = ["age" => 0, "key" => 10, "name" => "Oleg"];
+    echo $arr2["name"]
+    ?>
+
+</body>
+
+</html>

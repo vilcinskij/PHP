43aa74fcd7c6c17f70a67160d32121b08a69b6e6
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-16 20:52:00 +0300

    include_once header

---
+++ php-example/blocks/header.php
@@ -0,0 +1,7 @@
+<header>
+    <a href="../index.php">Main</a>
+    <a href="../arrays.php">Arrays</a>
+    <a href="../functions.php">Functions</a>
+    <a href="../loop.php">Loop</a>
+    <a href="../switch.php">Switch</a>
+</header>
--- php-example/index.php
+++ php-example/index.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>INDEX</title>
 </head>
-<header>
-    <a href="/">Main</a>
-    <a href="/arrays.php">Arraysas</a>
-</header>
+
+<?php 
+include_once "./blocks/header.php"
+?>
 
 <body>
     <?php
